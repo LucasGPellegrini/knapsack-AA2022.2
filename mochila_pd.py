33fbe7b0cb3d57c9984f19c68e664f1c09b99c8b
Lucas Guerreiro Pellegrini <92069804+LucasGPellegrini@users.noreply.github.com>
2022-12-27 18:48:46 -0300

    mochila com programação dinâmica added

---
+++ mochila_pd.py
@@ -0,0 +1,42 @@
+import numpy as np
+
+PESO = 1
+VALOR = 0
+
+
+def main():
+    lista_coisas = [(10, 5), (40, 4), (30, 6), (50, 3)]
+    mochila = MochilaPD()
+
+    mochila.resolve(10, lista_coisas)
+    print(f'Resposta mochila_prog_dinam: {mochila.resposta}')
+
+
+class MochilaPD:
+    def __init__(self):
+        self.matriz = [[]]
+        self.resposta = -1
+
+    def resolve(self, k, coisas):
+        self.matriz = np.zeros((len(coisas), k+1))
+
+        for i in range(len(coisas)):
+            for c in range(0, k+1):
+                if coisas[i - 1][PESO] > c:
+                    self.matriz[i][c] = self.matriz[i - 1][c]
+                else:
+                    menorSem = self.matriz[i - 1][c]
+                    menorCom = self.matriz[i - 1][c - coisas[i - 1][PESO]] + coisas[i - 1][VALOR]
+                    self.matriz[i][c] = max(menorSem, menorCom)
+
+        self.acha_maior()
+
+    def acha_maior(self):
+        for linha in self.matriz:
+            for elem in linha:
+                if elem > self.resposta:
+                    self.resposta = elem
+
+
+if __name__ == "__main__":
+    main()
