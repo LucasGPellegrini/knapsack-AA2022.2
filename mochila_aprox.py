ea8a1e80ee26a445d9bd7370f20b016b72cc9d37
Lucas Guerreiro Pellegrini <92069804+LucasGPellegrini@users.noreply.github.com>
2023-01-28 18:40:12 -0300

    IbaraKi fix???

--- mochila_aprox.py
+++ mochila_aprox.py
@@ -1,6 +1,3 @@
-import tracemalloc
-import numpy as np
-
 PESO = 1
 VALOR = 0
 
@@ -10,8 +7,8 @@ class MochilaA:
         self.matriz = [[]]
         self.resposta = -1
 
-    def resolve(self, k, coisas):
-        self.pre_processamento_IK(k, coisas)
+    def resolve(self, k, coisas, epsilon = 0.15):
+        coisas = self.pre_processamento_IK(k, coisas, epsilon)
 
         self.matriz = np.zeros((len(coisas), k+1))
 
@@ -32,16 +29,18 @@ class MochilaA:
                 if elem > self.resposta:
                     self.resposta = elem
 
-    def pre_processamento_IK(self, k, coisas):
+    def pre_processamento_IK(self, k, coisas, epsilon):
         # Remove elementos com peso maior que a capacidade
-        coisas[:] = [x for x in coisas if x[PESO] < k]
+        coisas[:] = [x for x in coisas if x[PESO] <= k]
 
         # IBARRA-KIM
-        sigma = max(coisas, key=lambda x: x[VALOR])[VALOR]
-        epsilon = np.random.ranf()
+        sigma = max(coisas)[VALOR]
+        #epsilon = np.random.ranf()
         print(f"\u03B5 = {epsilon}")
-        lmbd = (epsilon * sigma) / len(coisas)
+        lmbd = (epsilon * sigma) / (len(coisas)-1)
 
         u = []
-        for valor, _ in coisas:
-            u.append(valor/lmbd)
+        for valor, peso in coisas:
+            u.append(tuple((int(math.floor(valor/lmbd)), peso)))
+            
+        return u
